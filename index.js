1fed17c604f5f254dfb3c190eeacac020944e028
Priyam8Saxena <priyamsaxena2k@gmail.com>
2022-11-07 19:49:50 +0530

    Merge bug fixed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
+const dotenv = require("dotenv");
+
 const app = express()
+
+dotenv.config({ path: "./config.env" });
 
 const bikeRoute = require('./router/bikes.route')
 
 app.use("/api/",bikeRoute)
 
-const PORT = process.env.PORT | 3031;
+const PORT = process.env.PORT;
 
 app.listen(PORT,() => {
     console.log("Server is running on port " + PORT)
